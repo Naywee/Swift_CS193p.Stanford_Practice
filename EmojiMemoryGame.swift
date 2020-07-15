0a6c6b265faff6f65e2970774b2211a607fc67e8
Naiwen Zhang <68291152+Naywee@users.noreply.github.com>
2020-07-15 20:55:51 +0800

    Add files via upload

---
+++ EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize_Practice
+//
+//  Created by 张乃文 on 2020/7/15.
+//  Copyright © 2020 张乃文. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+}
